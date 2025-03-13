6cd9eb530b0e1915da71be92ed8bfa1ebc3cbd3d
Waleeeeed88 <fifawaleed4@gmail.com>
2025-03-12 23:35:02 -0400

    Update UserLogin.java

--- ParkingApp/src/main/java/com/parkingapp/UserLogin.java
+++ ParkingApp/src/main/java/com/parkingapp/UserLogin.java
@@ -1,4 +1,5 @@
-package com.parkingapp;
+
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -304,3 +305,4 @@ public class UserLogin {
         return response.toString();
     }
 }
+
